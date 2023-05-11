684a64efef6dc4f2c014f2f363f09738decccd1d
Chaitanya Jadhav <chaitanyajadhav634@gmail.com>
2023-05-11 13:07:14 +0530

    changed ui

--- App.tsx
+++ App.tsx
@@ -4,38 +4,57 @@ import {
   Button,
   SafeAreaView,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
 import {launchImageLibrary} from 'react-native-image-picker';
 import { VESDK } from "react-native-videoeditorsdk";
 
-function App(): JSX.Element {
+const launchVideoEditor = () => launchImageLibrary(
+  {'mediaType':'video'},
+  (res) => {
+    if(res.assets == null) {
+      return
+    }
+    const video = res.assets[0];
+    VESDK.openEditor(video.uri!).then((res) => {
+      if(res == null) {
+        return
+      }
+      console.log('====================================');
+      console.log(res);
+      console.log('====================================');
+    })
+  }
+)
 
+
+function App(): JSX.Element {
   return (
     <SafeAreaView style={{
       flex: 1,
       justifyContent: 'center',
-      alignSelf:'center'
+      alignSelf:'center',
     }} >
-      <Button 
-        title='Pick & Edit Video'
-        onPress={()=>{
-          launchImageLibrary({'mediaType':'video'},(res)=>{
-            if(res.assets == null) {
-              return
-            }
-            const video = res.assets[0];
-            VESDK.openEditor(video.uri!).then((res) => {
-              if(res == null) {
-                return
-              }
-              console.log('====================================');
-              console.log(res);
-              console.log('====================================');
-            })
-          })
-      }} />
+      <Text style={{textAlign:'center', fontSize: 32, color: '#85929E', fontWeight:'500'}} >
+        Intoactive
+      </Text>
+      <Text style={{textAlign:'center', color: '#85929E', fontSize: 18, marginBottom: 42}} >
+        Video Editor
+      </Text>
+      <TouchableOpacity 
+        style={{
+          backgroundColor: '#58D68D',
+          paddingHorizontal: 12,
+          paddingVertical: 8,
+          borderRadius: 12,
+          elevation: 2
+        }}
+        onPress={launchVideoEditor}
+      >
+        <Text style={{fontWeight:'bold', textAlign:'center'}} >Pick & Edit Video</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
